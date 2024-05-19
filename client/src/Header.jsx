372403779db13a98cbe756490f8fb1ee8bebe8f0
anjkk01 <121030480+anjkk01@users.noreply.github.com>
2024-05-19 14:45:33 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
         </svg>
-        <span className='font-bold text-xl'>TravelNest</span>
+        <span className='font-bold text-xl'>HotelGO</span>
       </Link>
       <div className='hidden md:flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300'>
         <div>Anywhere</div>
